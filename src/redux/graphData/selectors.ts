d05e6aedbfa22833c39dc4677b40ff2c418958da
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-06 23:51:00 +0300

    configured basic selector

---
+++ src/redux/graphData/selectors.ts
@@ -0,0 +1,7 @@
+import { RootState } from "./../store";
+
+export const selectGetUser = (state: RootState) => state.user.user;
+export const selectGetUserFavorite = (state: RootState) => state.user.user.favorites;
+export const selectUserError = (state: RootState) => state.user.error;
+export const selectUserIsLoading = (state: RootState) => state.user.isLoadingUser;
+export const selectAvatarIsLoading = (state: RootState) => state.user.isLoadingAvatar;
