dca732512cc8182dd37844afea45c1abd796f711
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-08 23:56:46 +0300

    Update Row1.tsx

--- src/pages/dashboard/Row1.tsx
+++ src/pages/dashboard/Row1.tsx
@@ -1,13 +1,16 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "@/redux/graphData/operations";
 
 const Row1 = () => {
-  return (
-    <>
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-    </>
-  );
+	const { data } = useGetKpisQuery();
+
+	return (
+		<>
+			<DashboardBox gridArea="a"></DashboardBox>
+			<DashboardBox gridArea="b"></DashboardBox>
+			<DashboardBox gridArea="c"></DashboardBox>
+		</>
+	);
 };
 
 export default Row1;
