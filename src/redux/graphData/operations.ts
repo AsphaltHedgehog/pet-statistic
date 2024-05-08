9c36428c43497eee590cbbfc99a15978b87f13e2
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-08 23:57:26 +0300

    refactured operations
    used creatiApi instead of Thunk

--- src/redux/graphData/operations.ts
+++ src/redux/graphData/operations.ts
@@ -1,33 +1,15 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-// import { AxiosResponse } from "axios";
-import { IPassedQuizzes } from "@/redux/graphData/slice";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-interface UserInfo {
-	name: string;
-	email: string;
-	_id: string;
-	avatarURL: string;
-	favorite: string[];
-	passedQuizzes?: IPassedQuizzes[];
-	averageSuccess?: string;
-	average: number;
-	totalQuestions: number;
-	totalAnswers: number;
-}
-interface IResponse {
-	code: number;
-	data: { user: UserInfo };
-	status: string;
-}
-
-export const getUserThunk = createAsyncThunk<UserInfo, void>("getUserInfo", async (_, thunkApi) => {
-	try {
-		const { data }: AxiosResponse<IResponse> = await quizApi.get("user/info");
-		return data.data.user;
-	} catch (error) {
-		if (error instanceof Error && typeof error.message === "string") {
-			return thunkApi.rejectWithValue(error.message);
-		}
-		return thunkApi.rejectWithValue("An unknown error occurred");
-	}
+export const api = createApi({
+	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+	reducerPath: "main",
+	tagTypes: ["Kpis"],
+	endpoints: (build) => ({
+		getKpis: build.query<void, void>({
+			query: () => "kpi/kpis/",
+			providesTags: ["Kpis"],
+		}),
+	}),
 });
+
+export const { useGetKpisQuery } = api;
