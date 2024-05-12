56a4ab0a22e7de0034184a367bc2548f159c4480
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-13 00:32:27 +0300

    added types for api response

---
+++ src/redux/types.ts
@@ -0,0 +1,35 @@
+export interface IGetKpisResponse {
+	_id: string;
+	id: string;
+	__v: string;
+	totalProfit: number;
+	totalRevenue: number;
+	totalExpenses: number;
+	expensesByCategory: IExpensesByCategory;
+	mounthlyData: IMouth[];
+	dailyData: IDay[];
+}
+
+export interface IExpensesByCategory {
+	salaries: number;
+	supplies: number;
+	services: number;
+}
+
+export interface IMouth {
+	_id: string;
+	id: string;
+	mouth: string;
+	revenue: number;
+	expenses: number;
+	nonOperationalExpenses: number;
+	operationalExpenses: number;
+}
+
+export interface IDay {
+	_id: string;
+	id: string;
+	date: string;
+	revenue: number;
+	expenses: number;
+}
