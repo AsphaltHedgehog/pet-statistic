ba0c824ea0e6af8821d9d9a2148110879cfe9f7b
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-13 00:32:53 +0300

    cleared unused RTK files

--- src/redux/graphData/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from "@/redux/store";
-
-export const selectGetGraphData = (state: RootState) => state.graphData.data;
-export const selectGraphDataError = (state: RootState) => state.graphData.error;
-export const selectGraphDataIsLoading = (state: RootState) => state.graphData.isLoadingUser;
