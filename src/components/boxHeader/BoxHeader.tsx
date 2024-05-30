b2699a7aa333cd430b2511c4768b2a82855c686e
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-31 00:19:58 +0300

    completed BoxHeader

--- src/components/boxHeader/BoxHeader.tsx
+++ src/components/boxHeader/BoxHeader.tsx
@@ -1,9 +1,31 @@
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "../FlexBetween";
 
-const BoxHeader = () => {
+interface IBoxHeaderProps {
+	title: string;
+	icon?: React.ReactNode;
+	subtitle?: string;
+	sideText?: string;
+}
+
+const BoxHeader = ({ icon, title, subtitle, sideText }: IBoxHeaderProps) => {
 	const { palette } = useTheme();
-	return <FlexBetween color={palette.grey[400]}></FlexBetween>;
+	return (
+		<FlexBetween color={palette.grey[400]} margin="1.5rem 1rem 0 1rem">
+			<FlexBetween>
+				{icon}
+				<Box width="100%">
+					<Typography variant="h4" mb="-0.1rem">
+						{title}
+					</Typography>
+					<Typography variant="h6">{subtitle}</Typography>
+				</Box>
+			</FlexBetween>
+			<Typography variant="h5" fontWeight="700" color={palette.secondary[500]}>
+				{sideText}
+			</Typography>
+		</FlexBetween>
+	);
 };
 
 export default BoxHeader;
