1e6c06f2357d1703a9eb25dee1e628de2d927ca4
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-05 20:35:15 +0300

    completed row1

--- src/pages/dashboard/Row1.tsx
+++ src/pages/dashboard/Row1.tsx
@@ -14,6 +14,8 @@ import {
 	CartesianGrid,
 	Legend,
 	LineChart,
+	Bar,
+	BarChart,
 } from "recharts";
 
 const Row1 = () => {
@@ -42,6 +44,18 @@ const Row1 = () => {
 					name: month.substring(0, 3),
 					revenue: revenue,
 					profit: (revenue - expenses).toFixed(2),
+				};
+			})
+		);
+	}, [data]);
+
+	const revenue = useMemo(() => {
+		return (
+			data &&
+			data.kpis[0].monthlyData.map(({ month, revenue }) => {
+				return {
+					name: month.substring(0, 3),
+					revenue: revenue,
 				};
 			})
 		);
@@ -137,8 +151,38 @@ const Row1 = () => {
 					</ResponsiveContainer>
 				)}
 			</DashboardBox>
-
-			<DashboardBox gridArea="c"></DashboardBox>
+			<DashboardBox gridArea="c">
+				<BoxHeader
+					title="Revenue Month by Month"
+					subtitle="graph representing the revenue month by month"
+					sideText="+4%"
+				/>
+				<ResponsiveContainer width="100%" height="100%" maxHeight={274}>
+					<BarChart
+						width={500}
+						height={300}
+						data={revenue}
+						margin={{
+							top: 17,
+							right: 15,
+							left: -5,
+							bottom: 58,
+						}}
+					>
+						<defs>
+							<linearGradient id="colorRevenueBars" x1="0" y1="0" x2="0" y2="1">
+								<stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.9} />
+								<stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+							</linearGradient>
+						</defs>
+						<CartesianGrid vertical={false} stroke={palette.grey[800]} />
+						<XAxis dataKey="name" axisLine={false} tickLine={false} style={{ fontSize: "10px" }} />\
+						<YAxis axisLine={false} tickLine={false} style={{ fontSize: "10px" }} />
+						<Tooltip />
+						<Bar dataKey="revenue" fill="url(#colorRevenueBars)" />
+					</BarChart>
+				</ResponsiveContainer>
+			</DashboardBox>
 		</>
 	);
 };
