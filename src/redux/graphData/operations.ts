fdb0bacfa10e17882ebd546a05197b616f3ee259
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-06 22:49:23 +0300

    added new Api request

--- src/redux/graphData/operations.ts
+++ src/redux/graphData/operations.ts
@@ -1,16 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { IGetKpisResponseData } from "../types";
+import { IGetKpisResponse, IGetProductsResponse } from "../types";
 
 export const api = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
 	reducerPath: "main",
-	tagTypes: ["Kpis"],
+	tagTypes: ["Kpis", "Product"],
 	endpoints: (build) => ({
-		getKpis: build.query<IGetKpisResponseData, void>({
+		getKpis: build.query<IGetKpisResponse, void>({
 			query: () => "kpi/",
 			providesTags: ["Kpis"],
+		}),
+		getProducts: build.query<Array<IGetProductsResponse>, void>({
+			query: () => "product/",
+			providesTags: ["Product"],
 		}),
 	}),
 });
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery } = api;
