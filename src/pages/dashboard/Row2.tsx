89e7d9914fe4b2a7bb042623c7e46788aa0d9885
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-08 01:23:43 +0300

    working on Row2

--- src/pages/dashboard/Row2.tsx
+++ src/pages/dashboard/Row2.tsx
@@ -1,45 +1,139 @@
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import DashboardBox from "@/components/DashboardBox";
 import BoxHeader from "@/components/boxHeader/BoxHeader";
-import { useGetProductsQuery } from "@/redux/graphData/operations";
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { useGetKpisQuery, useGetProductsQuery } from "@/redux/graphData/operations";
+import {
+  CartesianGrid,
+  Cell,
+  Line,
+  LineChart,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from "recharts";
+import { useMemo } from "react";
+import FlexBetween from "@/components/FlexBetween";
+
+// Mocked data for PieChart
+const pieData = [
+  { name: "Group A", value: 600 },
+  { name: "Group B", value: 250 }
+];
 
 const Row2 = () => {
-	const { palette } = useTheme();
-	const { data } = useGetProductsQuery();
-	console.log("data:", data);
+  const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[300]];
+  const { data: operationalData } = useGetKpisQuery();
+  const { data: productData } = useGetProductsQuery();
+  console.log("data:", productData);
 
-	return (
-		<>
-			<DashboardBox gridArea="d">
-				<BoxHeader title="Profit and Revenue" sideText="+4%" />
-				<ResponsiveContainer width="100%" height="100%" maxHeight={392}>
-					<LineChart
-						width={500}
-						height={400}
-						data={revenueProfit}
-						margin={{
-							top: 20,
-							right: 0,
-							left: -10,
-							bottom: window.innerWidth > 1199 ? -20 : 35,
-						}}
-					>
-						<CartesianGrid vertical={false} stroke={palette.grey[800]} />
-						<XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }} />
-						<YAxis yAxisId="left" tickLine={false} style={{ fontSize: "10px" }} axisLine={false} />
-						<YAxis yAxisId="right" orientation="right" tickLine={false} style={{ fontSize: "10px" }} axisLine={false} />
-						<Tooltip />
-						<Legend height={20} wrapperStyle={{ margin: "0 0 10px 0" }} />
-						<Line yAxisId="left" type="monotone" dataKey="profit" stroke={palette.tertiary[500]} />
-						<Line yAxisId="right" type="monotone" dataKey="revenue" stroke={palette.primary.main} />
-					</LineChart>
-				</ResponsiveContainer>
-			</DashboardBox>
-			<DashboardBox gridArea="e"></DashboardBox>
-			<DashboardBox gridArea="f"></DashboardBox>
-		</>
-	);
+  const operationalExpenses = useMemo(() => {
+    return (
+      operationalData &&
+      operationalData.monthlyData.map(({ month, operationalExpenses, nonOperationalExpenses }) => {
+        return {
+          name: month.substring(0, 3),
+          "Operational Expenses": operationalExpenses,
+          // prettier-ignore
+          "Non-Operational Expenses": nonOperationalExpenses
+        };
+      })
+    );
+  }, [operationalData]);
+
+  return (
+    <>
+      <DashboardBox gridArea="d">
+        {operationalExpenses && (
+          <>
+            <BoxHeader title="Operational vs Non-Operational Expenses" sideText="+4%" />
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                width={500}
+                height={400}
+                data={operationalExpenses}
+                margin={{
+                  top: 20,
+                  right: 0,
+                  left: -10,
+                  bottom: window.innerWidth > 1199 ? 40 : 35
+                }}
+              >
+                <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+                <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }} />
+                <YAxis
+                  yAxisId="left"
+                  orientation="left"
+                  tickLine={false}
+                  style={{ fontSize: "10px" }}
+                  axisLine={false}
+                />
+                <YAxis
+                  yAxisId="right"
+                  orientation="right"
+                  tickLine={false}
+                  style={{ fontSize: "10px" }}
+                  axisLine={false}
+                />
+                <Tooltip />
+                <Line
+                  yAxisId="left"
+                  type="monotone"
+                  dataKey="Non-Operational Expenses"
+                  stroke={palette.tertiary[500]}
+                />
+                <Line yAxisId="right" type="monotone" dataKey="Operational Expenses" stroke={palette.primary.main} />
+              </LineChart>
+            </ResponsiveContainer>
+          </>
+        )}
+      </DashboardBox>
+      <DashboardBox gridArea="e">
+        {pieData && (
+          <>
+            <BoxHeader title="Campaigns and Targets" sideText="+4%" />
+            <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+              <PieChart
+                width={110}
+                height={100}
+                margin={{
+                  top: 0,
+                  right: -10,
+                  left: 10,
+                  bottom: 0
+                }}
+              >
+                <Pie stroke="none" data={pieData} innerRadius={18} outerRadius={38} paddingAngle={0} dataKey="value">
+                  {pieData.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={pieColors[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
+              <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+                <Typography variant="h5">Target Sales</Typography>
+                <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>
+                  124
+                </Typography>
+                <Typography variant="h6">Finance goals of the campaign that is desired</Typography>
+              </Box>
+              <Box flexBasis="40%">
+                <Typography variant="h5">Loses in Revenue</Typography>
+                <Typography variant="h6">Losses are down 25%</Typography>
+                <Typography mt="0.4rem" variant="h5">
+                  Profit Margins
+                </Typography>
+                <Typography variant="h6">Margins are up by 30% from last month</Typography>
+              </Box>
+            </FlexBetween>
+          </>
+        )}
+      </DashboardBox>
+      <DashboardBox gridArea="f"></DashboardBox>
+    </>
+  );
 };
 
 export default Row2;
