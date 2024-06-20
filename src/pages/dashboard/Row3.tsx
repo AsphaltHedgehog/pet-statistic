bf0d811f5f84586753583c39fdf9e915d9f4904d
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-21 00:21:46 +0300

    Update Row3.tsx

--- src/pages/dashboard/Row3.tsx
+++ src/pages/dashboard/Row3.tsx
@@ -1,6 +1,11 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useGetTransactionsQuery } from "@/redux/graphData/operations";
+import BoxHeader from "@/shared/boxHeader/BoxHeader";
 
 const Row3 = () => {
+  const { data: transactionsData } = useGetTransactionsQuery();
+  console.log("data:", transactionsData);
+
   return (
     <>
       <DashboardBox gridArea="g"></DashboardBox>
